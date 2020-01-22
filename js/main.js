13bfcb71f10ed097de5239d4df96640b784a73af
pabpabpab <urallove@yandex.ru>
2020-01-23 02:25:52 +0500

    lesson5

--- js/main.js
+++ js/main.js
@@ -1,249 +1,73 @@
-'use strict';
-
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+const app = new Vue({
+  el: '#app',
 
-//========================================<промисифицировать getRequest>======================================
-let getRequest = (url) => {
-  return new Promise(function(resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status !== 200) {
-          reject('Error');
-        } else {
-          resolve(xhr.responseText);
-        }
-      }
-    };
-    xhr.send();
-  });
-};
+  data: {
+    catalogUrl: '/catalogData.json',
+    products: [ //товары с сервера
+      {id_product: 1000001, price: 3000, product_name: 'ИБП'},
+      {id_product: 1000002, price: 1000, product_name: 'Клавиатура'},
+      {id_product: 1000003, price: 10000, product_name: 'Сканер'},
+    ],
+    filteredProducts: [], // отфильтрованные товары
+    cartProducts: [], // товары в корзине
+    imgCatalog: 'https://placehold.it/200x150',
+    searchLine: '',
+    isVisibleCart: false,
+    //isEmptyCart: true,
+  },
 
+  methods: {
+    getJson(url) {
+      return fetch(url)
+          .then(result => result.json())
+          .catch(error => {
+            console.log(error);
+          });
+    },
 
-getRequest(`${API}/catalogData.json`)
-    .then(result => JSON.parse(result))
-    .then(result => console.log(result))
-    .catch(error => {
-      console.log(error);
-    });
+    addProduct(product) {
+      if (this._getGoodIndex(product, this.cartProducts) === -1) this.cartProducts.push(product);
+      this.isVisibleCart = true;
+    },
 
-//========================================</промисифицировать getRequest>======================================
+    deleteProduct(product) {
+      let goodIndex = this._getGoodIndex(product, this.cartProducts);
+      if (goodIndex === -1) return;
+      this.cartProducts.splice(goodIndex, 1);
+      this.isVisibleCart = true;
+    },
 
+    _getGoodIndex(product, products) {
+      return products.indexOf(product);
+    },
 
+    isEmptyCart() {
+      return !Boolean(this.cartProducts.length);
+    },
 
+    calcTotalPrice() {
+      return this.cartProducts.reduce((totalPrice, product) => totalPrice + product.price, 0);
+    },
 
+    filterGoods() {
+      const regexp = new RegExp(this.searchLine, 'i');
+      this.filteredProducts = this.products.filter(product => regexp.test(product.product_name));
+    },
 
-//=============================================<class ProductList>=============================================
-class ProductList {
+  },
 
-  constructor(container = '.products') {
-    this.container = container;
-    this.goods = [];
-    this.allProducts = [];
-    this._getProducts()
+  mounted() {
+    this.getJson(`${API + this.catalogUrl}`)
         .then(data => {
-          this.goods = [...data];;
-          this.render();
+
+          for (let el of data) {
+            this.products.push(el);
+          }
+
+          this.filteredProducts = [...this.products];
         });
-  }
+  },
 
-
-  _getProducts() {
-    return fetch(`${API}/catalogData.json`)
-        .then(result => result.json())
-        .catch(error => {
-          console.log(error);
-        });
-  }
-
-
-  render() {
-    const block = document.querySelector(this.container);
-    for (let product of this.goods) {
-      const productObject = new ProductItem(product);
-      this.allProducts.push(productObject);
-      block.insertAdjacentHTML('beforeend', productObject.render());
-    }
-    block.insertAdjacentHTML('beforebegin', '<div class="products__header">Список товаров</div>');
-  }
-
-
-  //выдать данные товара для корзины по кнопке Купить
-  getGoodsItem(id_product) {
-    let index = this._getGoodIndexById(id_product);
-    return this.goods[index];
-  }
-
-
-  _getGoodIndexById(id_product) {
-    //return this.goods.indexOf(`id_product: ${id_product}`);//это не работает, я не знаю как с помощью indexof делать поиск в массиве объектов
-
-    let index = -1;
-    for (let i = 0; i < this.goods.length; i++) {
-      if (id_product === this.goods[i].id_product) {
-        index = i;
-        break;
-      }
-    }
-    return index;
-  }
-
-}
-//=============================================</class ProductList>=============================================
-
-
-
-
-//=============================================<class ProductItem>=============================================
-class ProductItem {
-
-  constructor(product, img = 'https://placehold.it/200x150') {
-    this.title = product.product_name;
-    this.price = product.price;
-    this.id = product.id_product;
-    this.img = img;
-  }
-
-  render() {
-    return `<div class="product-item" data-id="${this.id}">
-                <img src="${this.img}" alt="Some img">
-                <div class="desc">
-                    <h3>${this.title}</h3>
-                    <p class="product__price">${this.price} \u20bd</p>
-                    <button data-buy-id="${this.id}" class="buy-btn">Купить</button>
-                </div>
-            </div>`;
-  }
-}
-//=============================================</class ProductItem>=============================================
-
-
-
-
-//==================================================<class Cart>================================================
-class Cart {
-
-  constructor(container = '.cart') {
-    this.container = container;
-    this.goods = [];
-  }
-
-  addProduct(product, img = 'https://placehold.it/200x150') {
-    if (!product.img) product.img = img;
-    if (this._getGoodIndexById(product.id_product) !== -1) return;
-    return this.goods.push(product);
-  }
-
-
-
-  deleteProduct(id_product) {
-    let index = this._getGoodIndexById(id_product);
-    return this.goods.splice(index, 1);
-  }
-
-
-
-  _getGoodIndexById(id_product) {
-    //return this.goods.indexOf(`id_product: ${id_product}`);//это не работает, я не знаю как с помощью indexof делать поиск в массиве объектов
-
-    let index = -1;
-    for (let i = 0; i < this.goods.length; i++) {
-      if (id_product === this.goods[i].id_product) {
-        index = i;
-        break;
-      }
-    }
-    return index;
-  }
-
-
-
-  _renderProduct = product => `<div class="product-item" data-id="${product.id_product}">
-                <img src="${product.img}" alt="Some img">
-                <div class="desc">
-                    <h3>${product.product_name}</h3>
-                    <p class="product__price">${product.price} \u20bd</p>
-                    <button data-delete-id="${product.id_product}" class="delete-btn">Удалить</button>
-                </div>
-            </div>`;
-
-
-
-
-  render() {
-    const block = document.querySelector(this.container);
-    block.innerHTML = '';
-
-
-    if (this.goods.length === 0) return block.insertAdjacentHTML('beforeend', 'Корзина пуста');;
-
-
-    const productList = this.goods.map(good => this._renderProduct(good));
-    const html = `Корзина <div class="cart__content">${productList.join('')}</div>`;
-
-
-    block.insertAdjacentHTML('beforeend', html);
-    block.insertAdjacentHTML('beforeend', this.renderTotalPrice());
-  }
-
-
-
-
-
-
-  _calcTotalPrice = goods => goods.reduce((total, currentItem) => total + currentItem.price, 0);
-
-  renderTotalPrice() {
-    return `<div class="products__totalPrice">Total price: ${this._calcTotalPrice(this.goods)} \u20bd</div>`;
-  }
-
-}
-//==================================================</class Cart>================================================
-
-
-
-
-
-
-
-function eventListener() {
-  let elem = document.querySelector('body');
-
-  elem.addEventListener('click', event => {
-    eventHandler(event);
-  });
-}
-
-
-
-
-//event handler
-function eventHandler(event) {
-  switch (true) {
-    case (Boolean(event.target.dataset.buyId)):
-      const good = catalog.getGoodsItem(+event.target.dataset.buyId);
-      cart.addProduct(good);
-      cart.render();
-      break;
-    case (Boolean(event.target.dataset.deleteId)):
-      cart.deleteProduct(+event.target.dataset.deleteId);
-      cart.render();
-      break;
-  }
-}
-
-
-
-
-const catalog = new ProductList();
-let cart = new Cart();
-eventListener();
-
-
-
-
-
-
-
+});
